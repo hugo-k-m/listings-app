5df732e17049b12d6abeb48fdfeb353f29ab336c
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-21 16:52:09 +0200

    Created root component and implemented basic global styles for the project.

--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -1,4 +1,28 @@
 import React from "react";
 import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-render(<h1>Working</h1>, document.getElementById("app"));
+import Root from "#root/components/Root";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+html, body, #app {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+
+body {
+    font-family: Roboto, sans-serif;
+}
+`;
+
+render(
+  <>
+    <GlobalStyle />
+    <Root />
+  </>,
+  document.getElementById("app")
+);
