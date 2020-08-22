3aee09cd6d5465e1f6abd9069c91c4963eca860d
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-22 14:32:22 +0200

    Implemented basic layout for Root.

--- classifieds-app/src/components/Root/Root.js
+++ classifieds-app/src/components/Root/Root.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+  width: 80rem;
+`;
+
+const Content = styled.div`
+  ${"" /* background-color: blue; */}
+  flex: 1;
+  margin-right: 1rem;
+`;
+
+const Sidebar = styled.div`
+  ${"" /* background-color: red; */}
+  flex: 0 auto;
+  width: 10rem;
+`;
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  padding: 1rem;
+  width: 100%;
+`;
 
 const Root = () => {
-  return <h1>Root</h1>;
+  return (
+    <Wrapper>
+      <Container>
+        <Content>Some content.</Content>
+        <Sidebar>More content.</Sidebar>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Root;
