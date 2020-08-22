74276dfc36d56380cc707f0ed3b42b9f95417cf6
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-22 16:22:40 +0200

    Configured the user login functionality to be able to be accessed via the API gateway.

--- api-gateway/src/adapters/UsersService.js
+++ api-gateway/src/adapters/UsersService.js
@@ -12,4 +12,14 @@ export default class UsersService {
 
     return body;
   }
+
+  static async createUserSession({ email, password }) {
+    const body = await got
+      .post(`${USERS_SERVICE_URI}/sessions`, {
+        json: { email, password },
+      })
+      .json();
+
+    return body;
+  }
 }
