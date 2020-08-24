8c3c2376aa51ec45b8e631b05417166da27506a6
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-24 17:49:29 +0200

    Built a route to fetch a user by their unique identifier.

--- users-service/src/server/routes.js
+++ users-service/src/server/routes.js
@@ -60,6 +60,18 @@ const setupRoutes = (app) => {
       return next(e);
     }
   });
+
+  app.get("/users/:userId", async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.userId);
+
+      if (!user) return next(new Error("Invalid user ID."));
+
+      return res.json(user);
+    } catch (error) {
+      return next(error);
+    }
+  });
 };
 
 export default setupRoutes;
