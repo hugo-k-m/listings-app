a3d1a8118fc0424306ac203a3fb8ea17fd46cb54
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-24 12:58:26 +0200

    Implemented the skeleton for the login form.

--- classifieds-app/src/components/Root/Root.js
+++ classifieds-app/src/components/Root/Root.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import Login from "./Login";
+
 const Container = styled.div`
   display: flex;
   flex-flow: row nowrap;
@@ -9,13 +11,11 @@ const Container = styled.div`
 `;
 
 const Content = styled.div`
-  ${"" /* background-color: blue; */}
   flex: 1;
   margin-right: 1rem;
 `;
 
 const Sidebar = styled.div`
-  ${"" /* background-color: red; */}
   flex: 0 auto;
   width: 10rem;
 `;
@@ -32,7 +32,9 @@ const Root = () => {
     <Wrapper>
       <Container>
         <Content>Some content.</Content>
-        <Sidebar>More content.</Sidebar>
+        <Sidebar>
+          <Login />
+        </Sidebar>
       </Container>
     </Wrapper>
   );
