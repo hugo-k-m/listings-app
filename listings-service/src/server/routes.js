e19dd0659990edd083cd6f0b74bf3e233640bc85
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-08-28 14:57:54 +0200

    Added route to post a listing. Implemented minimal error handling in the server.

--- listings-service/src/server/routes.js
+++ listings-service/src/server/routes.js
@@ -2,9 +2,30 @@ import { Listing } from "#root/db/models";
 
 const setupRoutes = (app) => {
   app.get("/listings", async (req, res, next) => {
-    const listings = await Listing.findAll();
+    try {
+      const listings = await Listing.findAll();
 
-    return res.json(listings);
+      return res.json(listings);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.post("/listings", async (req, res, next) => {
+    if (!req.body.description || !req.body.title) {
+      return next(new Error("Invalid body!"));
+    }
+
+    try {
+      const listing = await Listing.create({
+        description: req.body.description,
+        title: req.body.title,
+      });
+
+      return res.json(listing);
+    } catch (error) {
+      next(error);
+    }
   });
 };
 
